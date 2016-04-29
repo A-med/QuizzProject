412e69ba2a797e75e4d52d984f93c7e6db755ff9
Mahdi Kallel <Mahdi Kallel>
2016-04-29 01:03:35 +0100

    First commit from branch mahdi

---
+++ app/src/main/java/com/example/dmk/quizzproject/SunMoonView.java
@@ -0,0 +1,35 @@
+package onboarding.yahoo.com.yahoonewsonboarding;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.View;
+
+/**
+ * Created by rahul.raja on 9/19/15.
+ */
+public class SunMoonView extends View {
+
+
+  public SunMoonView(Context context) {
+        super(context);
+
+    }
+
+
+
+
+
+}
