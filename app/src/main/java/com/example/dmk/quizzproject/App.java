50a33b5219206e9e1c62a73afcfca38115347006
Nabil Moalla <Nabil Moalla>
2016-05-02 13:16:10 +0100

    question

---
+++ app/src/main/java/com/example/dmk/quizzproject/App.java
@@ -0,0 +1,23 @@
+package com.example.dmk.quizzproject;
+
+import android.app.Application;
+import android.graphics.Typeface;
+
+/**
+ * Created by Nabil Moalla on 20/04/2016.
+ */
+public class App extends Application {
+    private static final String CANARO_EXTRA_BOLD_PATH = "fonts/canaro_extra_bold.otf";
+    public static Typeface canaroExtraBold;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initTypeface();
+    }
+
+    private void initTypeface() {
+        canaroExtraBold = Typeface.createFromAsset(getAssets(), CANARO_EXTRA_BOLD_PATH);
+
+    }
+}
---
+++ app/src/main/java/com/example/dmk/quizzproject/PageFragment.java
@@ -0,0 +1,21 @@
+package com.example.dmk.quizzproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nabil Moalla on 24/04/2016.
+ */
+public class PageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Get the view from fragmenttab1.xml
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        return view;
+    }
+}
